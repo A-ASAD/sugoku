a68170fcd1e22a36796ed7f96cbf48fc8ff857a7
Abdurrahman Asad <abdurrahmanasad133@gmail.com>
2022-03-29 01:00:56 +0500

    fix: status not updating after changing difficulty

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,7 +5,8 @@ import Loader from '../../components/loader';
 import {
     updateBoard,
     updateCurrentDifficulty,
-    updateLoadingStatus
+    updateLoadingStatus,
+    updateStatus
 } from '../../store/actions';
 import { context } from '../../store/context';
 import { getBoardFromResponse, sendRequest } from './utils';
@@ -22,6 +23,7 @@ export default function Home() {
                 `https://vast-chamber-17969.herokuapp.com/generate?difficulty=${difficulty}`
             );
             dispatch(updateCurrentDifficulty(data.difficulty));
+            dispatch(updateStatus('unsolved'));
             const board = getBoardFromResponse(data.puzzle);
             dispatch(updateBoard(board));
             dispatch(updateLoadingStatus(false));
